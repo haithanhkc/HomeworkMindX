d61a34e3b8119472aef36b0910d849e9c9f00d1b
haithanhIT <haithanhkc09@gmail.com>
2023-08-28 13:13:46 +0700

    HWday1

--- ex4.js
+++ ex4.js
@@ -3,7 +3,12 @@
  */
 
 // Describe all steps
-
+// 1- git init
+// 2- git remote add origin
+// 3- git add .
+// 4- git remote add
+// 5- git commit -m "
+// 6- git push origin master
 
 // Your git url
-
+//https://github.com/haithanhkc/HomeworkMindX.git
